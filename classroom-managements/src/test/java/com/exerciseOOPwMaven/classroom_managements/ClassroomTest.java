9b9e88bbf0763255705eb8b8a2573aee7dc5ddce
loan chung <loan-chung@loans-MacBook-Air.local>
2021-03-22 22:26:11 +0100

    fix some tests

--- classroom-managements/src/test/java/com/exerciseOOPwMaven/classroom_managements/ClassroomTest.java
+++ classroom-managements/src/test/java/com/exerciseOOPwMaven/classroom_managements/ClassroomTest.java
@@ -7,29 +7,37 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class ClassroomTest {
-	ArrayList<Student> std = new ArrayList<Student>();
-	Classroom cl = new Classroom("MT", "Spring 2021", std);
 	
 	@Test
 	public void testAddANS() {
+		ArrayList<Student> std = new ArrayList<Student>();
+		Classroom cl = new Classroom("MT", "Spring 2021", std);
 		int x = std.size();
-		System.out.println(x);
+		//System.out.println(x);
 		Student std1 = new Student(7,"Meo","Jhs",'M',7.0,8.0,9.0,0.0);
 		cl.addANewStudent(std1);
 		int y = std.size();
-		System.out.println(y);
+		//System.out.println(y);
 		assertTrue(x < y);	
 	}
+	
 	@Test
 	public void testRemoveAS() {
-		String firstName = "Ti";
-		boolean removeE = true;
-		for (int i = 0; i < std.size(); i++) {
-			if (std.contains(firstName)) {
-				//return true;
-			}//else return false;
+		ArrayList<Student> std = new ArrayList<Student>();
+		Classroom cl = new Classroom("MT", "Spring 2021", std);
+		int x = std.size();
+		//System.out.println(x);
+		Student std1 = new Student(7,"Meo","Jhs",'M',7.0,8.0,9.0,0.0);
+		cl.addANewStudent(std1);
+		String firstName = "Meo";
+		cl.removeAStudent(firstName);
+		ArrayList<Student> std2 = cl.getStudents();
+		// check if std2 contains "Meo"
+		for (int i = 0; i < std2.size(); i++) {
+			if (std2.get(i).getFirstName().equals("Meo")) {
+				assertFalse(true);
+			}
 		}
-		assertTrue(!std.contains("Ti"));
 	}
 
 }
